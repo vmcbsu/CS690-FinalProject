4a7fffa2f515030b68cdbca3c7b79acbffd65f63
vmcbsu <vincent.mcconnell@bsu.edu>
2026-05-01 18:38:31 +0000

    newer

--- GardenHelper/ActivityLog.cs
+++ GardenHelper/ActivityLog.cs
@@ -2,8 +2,12 @@ namespace GardenHelper;
 public class ActivityLog{
     public int PlantId{get;set;}
     public ActivityType ActivityType{get;set;}
-    public ActivityLog(int plantId,ActivityType activityType){
+    public DateTime Timestamp{get;set;}
+    public string Comments{get;set;}
+    public ActivityLog(int plantId,ActivityType activityType,DateTime timestamp,string comments){
         PlantId = plantId;
         ActivityType = activityType;
+        Timestamp = timestamp;
+        Comments = comments;
     }
 }
